23eadd9b958624f82860d29084ec92b91718dd4f
Christian Chesire <christian.chesire@strathmore.edu>
2020-09-10 15:26:04 +0300

    Reactive Properties

--- Exercise 2/computed_properties.js
+++ Exercise 2/computed_properties.js
@@ -1,17 +1,25 @@
 new Vue ({
-	el : '#app',
+  el : '#app',
   data: {
-  	counter: 0,
+    counter: 0,
     secondCounter: 0
   },
   computed: {
-  	output: function(){
-    	console.log('Computed');
-    	return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+    output: function(){
+      console.log('Computed');
+      return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+    }
+  },
+  watch: {
+    counter: function(value){
+      var vm = this;
+      setTimeout(function (){
+        vm.counter = 0;
+      }, 2000);
     }
   },
   methods: {
-  	result: function(){
+    result: function(){
     console.log('Method')
     return this.counter > 5 ? 'Greater 5' : 'Smaller than 5 '
     }
